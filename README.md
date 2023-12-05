8ea4c599e3518fba1eca76e10b3d633cfd6f9188
pmason <pmason@argenx.com>
2023-12-05 14:53:36 +0000

    basicly working

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# View
+
 On the end of the app URL you can use parms to specify the file you want to view:
 
 ?lsaf=/general/biostat/jobs/dashboard/dev/output/sapxlsx/sap_updates.json
@@ -9,3 +11,8 @@ Ideas:
     - display changed values in a color so we know what has been changed
     - columns that can be edited can be specified via a parm
     - add CSV support to view and edit
+
+[10:19] Jean-Michel Bodart
+if you have columns with long text strings e.g. paths to show, and they all start with  the same text, it might be useful to make the text right-aligned in the cells so you can see the interesting part (=the last characters) without having to display the whole string by widening the column; would that be easily implementable ?
+[10:29] Phil Mason
+thats a good idea - should be easy to either right justify everything or work out what is over a length and do it to that. I might add some options that a user can specify and then save them to local storage
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   Box,
   Dialog,
@@ -11,11 +11,21 @@ import {
   MenuItem,
   Menu,
   // Link,
+  Snackbar,
   Button,
+  ListItemIcon,
+  ListItemText,
 } from "@mui/material";
 import {
   DataGridPro,
   LicenseInfo,
+  GridColumnMenu,
+  useGridApiContext,
+  gridColumnDefinitionsSelector,
+  // GridEditSingleSelectCell,
+  GridCellEditStopReasons,
+  // GridColumnMenuProps,
+  // GridColumnMenuItemProps,
   // GridToolbar,
   // GridRowModes,
   // GridToolbarContainer,
@@ -23,7 +33,11 @@ import {
   // GridRowEditStopReasons,
   // GridToolbarExport,
 } from "@mui/x-data-grid-pro";
-import { Info } from "@mui/icons-material";
+import {
+  Info,
+  AlignHorizontalLeft,
+  AlignHorizontalRight,
+} from "@mui/icons-material";
 import MenuIcon from "@mui/icons-material/Menu";
 import "./App.css";
 import localData from "./data.json";
@@ -39,7 +53,6 @@ function App() {
     //   "https://xarprod.ondemand.sas.com/lsaf/filedownload/sdd:/general/biostat/tools/fileviewer/index.html?file=",
     // logViewerPrefix =
     //   "https://xarprod.ondemand.sas.com/lsaf/webdav/repo/general/biostat/tools/logviewer/index.html",
-    variable = "123",
     handleClickMenu = (event) => {
       setAnchorEl(event.currentTarget);
     },
@@ -52,22 +65,166 @@ function App() {
         url: "https://xarprod.ondemand.sas.com/lsaf/filedownload/sdd%3A///general/biostat/tools/control/index.html",
       },
     ],
+    [openSnackbar, setOpenSnackbar] = useState(false),
+    handleCloseSnackbar = (event, reason) => {
+      if (reason === "clickaway") {
+        return;
+      }
+      setOpenSnackbar(false);
+    },
+    [message, setMessage] = useState(""), // message to display in snackbar
     [anchorEl, setAnchorEl] = useState(null),
     [openInfo, setOpenInfo] = useState(false),
     [rows, setRows] = useState([]),
     [cols, setCols] = useState([]),
-    [current, setCurrent] = useState(null);
+    [current, setCurrent] = useState(null),
+    [jsonUrl, setJsonUrl] = useState(null),
+    Align = (props) => {
+      const { myCustomHandler, align } = props;
+      return (
+        <MenuItem onClick={myCustomHandler}>
+          <ListItemIcon>
+            {align === "left" ? (
+              <AlignHorizontalLeft fontSize="small" />
+            ) : (
+              <AlignHorizontalRight fontSize="small" />
+            )}
+          </ListItemIcon>
+          <ListItemText>{align}</ListItemText>
+        </MenuItem>
+      );
+    },
+    CustomColumnMenu = (props) => {
+      const apiRef = useGridApiContext();
+      return (
+        <GridColumnMenu
+          {...props}
+          slots={{
+            right: Align,
+            left: Align,
+          }}
+          slotProps={{
+            right: {
+              align: "right",
+              myCustomHandler: (e) => {
+                const c = gridColumnDefinitionsSelector(apiRef);
+                c.forEach((col) => {
+                  col.headerAlign = "right";
+                  col.align = "right";
+                });
+                console.log(e, c);
+                // apiRef.current.align("", "right");
+              },
+            },
+            left: {
+              align: "left",
+              myCustomHandler: (e) => {
+                const c = gridColumnDefinitionsSelector(apiRef);
+                c.forEach((col) => {
+                  col.headerAlign = "left";
+                  col.align = "left";
+                });
+                console.log(e, c);
+                // apiRef.current.align("", "right");
+              },
+            },
+          }}
+        />
+      );
+    },
+    editingRow = useRef(null),
+    // CustomTypeEditComponent(props) {
+    //   const { setRows, ...other } = props;
+
+    //   const handleValueChange = () => {
+    //     setRows((prevRows) => {
+    //       console.log(prevRows);
+    //       return prevRows.map((row) =>
+    //         row.id === props.id ? { ...row, account: null } : row,
+    //       );
+    //     });
+    //   };
+
+    //   return <GridEditSingleSelectCell onValueChange={handleValueChange} {...other} />;
+    // },
+    handleCellEditStart = (params) => {
+      editingRow.current = rows.find((row) => row.id === params.id) || null;
+    },
+    handleCellEditStop = (params) => {
+      if (params.reason === GridCellEditStopReasons.escapeKeyDown) {
+        setRows((prevRows) =>
+          prevRows.map((row) =>
+            row.id === editingRow.current?.id
+              ? { ...row, account: editingRow.current?.account }
+              : row
+          )
+        );
+      }
+    },
+    processRowUpdate = (newRow) => {
+      console.log("newRow", newRow);
+      setRows((prevRows) =>
+        prevRows.map((row) => (row.id === newRow?.id ? newRow : row))
+      );
+      return newRow;
+    },
+    updateJsonFile = (file, content) => {
+      console.log("updateJsonFile - file:", file, "content:", content);
+      if (!file || !content) return;
+      // drop id from each row in content
+      const contentWithoutId = content.map((c) => {
+        delete c.id;
+        return c;
+      });
+      // try to delete the file, in case it is there already, otherwise the PUT will not work
+      fetch(file, {
+        method: "DELETE",
+      })
+        .then((response) => {
+          console.log("response", response);
+          fetch(file, {
+            method: "PUT",
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+            },
+            body: JSON.stringify(contentWithoutId),
+          })
+            .then((response) => {
+              setMessage(response.ok ? "File saved" : "File not saved");
+              setOpenSnackbar(true);
+              console.log("response", response);
+              response.text().then(function (text) {
+                console.log("text", text);
+              });
+            })
+            .catch((err) => {
+              setMessage(err);
+              setOpenSnackbar(true);
+              console.log("PUT err: ", err);
+            });
+        })
+        .catch((err) => {
+          setMessage(
+            "DELETE was attempted before the new version was saved - but the DELETE failed. (see console)"
+          );
+          setOpenSnackbar(true);
+          console.log("DELETE err: ", err);
+        });
+    };
 
   // fetch a JSON file to display in table
   useEffect(() => {
     if (mode === "local") {
-      setRows(localData);
+      setRows(localData.map((d, i) => ({ ...d, id: i }))); // add an id field to each row
       setCols(
         Object.keys(localData[0]).map((k) => ({
           field: k,
           headerName: k,
           editable: true,
         }))
+      );
+      setJsonUrl(
+        "https://xarprod.ondemand.sas.com/lsaf/webdav/repo/general/biostat/jobs/dashboard/dev/output/sapxlsx/sap_updates.json"
       );
     } else {
       const split = href.split("?"),
@@ -83,6 +240,7 @@ function App() {
       if ("lsaf" in parsed) {
         setCurrent(parsed.lsaf);
         url = `${webDavPrefix}${parsed.lsaf}`;
+        setJsonUrl(url);
       }
       console.log(
         "href",
@@ -99,13 +257,20 @@ function App() {
       fetch(url)
         .then((res) => res.json())
         .then((data) => {
-          setRows(data);
+          // setRows(data);
+          setRows(data.map((d, i) => ({ ...d, id: i }))); // add an id field to each row
           setCols(
-            Object.keys(data[0]).map((k) => ({ field: k, headerName: k }))
+            Object.keys(data[0]).map((k) => ({
+              field: k,
+              headerName: k,
+              editable: true,
+            }))
           );
         });
     }
   }, [href, mode]);
+
+  console.log("rows", rows, "cols", cols);
 
   return (
     <div className="App">
@@ -128,7 +293,13 @@ function App() {
           <Box color="inherit">JSON File Viewer</Box>
           <Box sx={{ flexGrow: 1 }}></Box>
           <Box sx={{ flexGrow: 0.5, fontSize: "0.8em", textAlign: "right" }}>
-            {current ? `Currently viewing ${current}` : ""}
+            {current ? (
+              <span>
+                <b>Currently viewing</b> {current}
+              </span>
+            ) : (
+              ""
+            )}
           </Box>
           <Tooltip title="Information about this screen">
             <IconButton
@@ -146,21 +317,41 @@ function App() {
       <DataGridPro
         columns={cols}
         rows={rows}
-        getRowId={(row) => Math.random() * 1000000}
+        rowHeight={22}
+        density="compact"
+        // getRowId={(row) => row.__id__}
         // autoHeight
         pageSizeOptions={[25, 100, 1000]}
         pagination
         editMode="row"
+        slots={{ columnMenu: CustomColumnMenu }}
         sx={{
           width: window.innerWidth - 100,
-          height: window.innerHeight - 100,
+          fontWeight: "fontSize=5",
+          fontSize: "0.7em",
+          padding: 1,
         }}
+        onCellEditStart={handleCellEditStart}
+        onCellEditStop={handleCellEditStop}
+        processRowUpdate={processRowUpdate}
       />
       <Tooltip title="Save JSON back to server">
-        <Button variant="contained" sx={{ mt: 1 }} onClick={() => {}}>
+        <Button
+          variant="contained"
+          sx={{ mt: 1 }}
+          onClick={() => {
+            updateJsonFile(jsonUrl, rows);
+          }}
+        >
           Save
         </Button>
       </Tooltip>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        message={message}
+      />
       <Menu
         anchorEl={anchorEl}
         id="link-menu"
@@ -199,7 +390,28 @@ function App() {
         <DialogTitle>Info about this screen</DialogTitle>
         <DialogContent>
           <ul>
-            <li>Currently under development ...</li>
+            <li>
+              <b>Double click</b> on a cell to edit that row.
+            </li>
+            <li>
+              Press <b>enter</b> to finish editing the row.
+            </li>
+            <li>
+              Press <b>escape</b> to cancel editing the row.
+            </li>
+            <li>
+              Click on the column header and then the <b>3 vertical dots</b> to
+              sort the column.
+            </li>
+            <li>
+              Click on the <b>SAVE</b> button to write the JSON file back to the
+              location it was loaded from.
+            </li>
+            <li>
+              Pressing <b>SAVE</b> will first delete the file that was there
+              (using HTTP DELETE), and then does an writes the file to server
+              (using HTTP PUT).
+            </li>
           </ul>
         </DialogContent>
       </Dialog>
